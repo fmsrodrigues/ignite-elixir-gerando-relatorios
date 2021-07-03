6a238995dfe599dd507b8e938b0fa2aa7725f63b
Filipe Maia <filipe_msr@hotmail.com>
2021-07-03 17:47:59 -0300

    Initial commit

---
+++ mix.exs
@@ -0,0 +1,34 @@
+defmodule GenReport.MixProject do
+  use Mix.Project
+
+  def project do
+    [
+      app: :gen_report,
+      elixirc_paths: elixirc_paths(Mix.env()),
+      version: "0.1.0",
+      elixir: "~> 1.11",
+      start_permanent: Mix.env() == :prod,
+      deps: deps()
+    ]
+  end
+
+  # Run "mix help compile.app" to learn about applications.
+  def application do
+    [
+      extra_applications: [:logger]
+    ]
+  end
+
+  # Specifies which paths to compile per environment.
+  defp elixirc_paths(:test), do: ["lib", "test/support"]
+  defp elixirc_paths(_), do: ["lib"]
+
+  # Run "mix help deps" to learn about dependencies.
+  defp deps do
+    [
+      {:credo, "~> 1.5", only: [:dev, :test], runtime: false}
+      # {:dep_from_hexpm, "~> 0.3.0"},
+      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+    ]
+  end
+end
