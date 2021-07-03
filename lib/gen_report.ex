6d0d6269030be53000f057dd7465480cd4254e0d
f-maia <filipe_msr@hotmail.com>
2021-07-03 19:02:08 -0300

    Challenge solution

--- lib/gen_report.ex
+++ lib/gen_report.ex
@@ -1,6 +1,83 @@
 defmodule GenReport do
+  alias GenReport.Parser
+
+  @names [
+    "daniele",
+    "mayk",
+    "giuliano",
+    "cleiton",
+    "jakeliny",
+    "joseph",
+    "diego",
+    "danilo",
+    "rafael",
+    "vinicius"
+  ]
+
+  @months [
+    "janeiro",
+    "fevereiro",
+    "março",
+    "abril",
+    "maio",
+    "junho",
+    "julho",
+    "agosto",
+    "setembro",
+    "outubro",
+    "novembro",
+    "dezembro"
+  ]
 
   def build do
-    # TO DO
+    {:error, "Insira o nome de um arquivo"}
+  end
+
+  def build(filename) do
+    filename
+    |> Parser.parse_file()
+    |> Enum.reduce(report_acc(), fn line, report -> sum_values(line, report) end)
+  end
+
+  defp sum_values([name, hours, _day, month, year], %{
+         "all_hours" => all_hours,
+         "hours_per_month" => hours_per_month,
+         "hours_per_year" => hours_per_year
+       }) do
+    all_hours = Map.put(all_hours, name, all_hours[name] + hours)
+
+    hours_per_month =
+      Map.put(
+        hours_per_month,
+        name,
+        Map.put(hours_per_month[name], month, hours_per_month[name][month] + hours)
+      )
+
+    hours_per_year =
+      Map.put(
+        hours_per_year,
+        name,
+        Map.put(hours_per_year[name], year, hours_per_year[name][year] + hours)
+      )
+
+    build_report(all_hours, hours_per_month, hours_per_year)
+  end
+
+  defp report_acc do
+    all_hours = Enum.into(@names, %{}, &{&1, 0})
+    empty_months = Enum.into(@months, %{}, &{&1, 0})
+    hours_per_month = Enum.into(@names, %{}, &{&1, empty_months})
+    empty_years = Enum.into(2016..2020, %{}, &{&1, 0})
+    hours_per_year = Enum.into(@names, %{}, &{&1, empty_years})
+
+    build_report(all_hours, hours_per_month, hours_per_year)
+  end
+
+  defp build_report(all_hours, hours_per_month, hours_per_year) do
+    %{
+      "all_hours" => all_hours,
+      "hours_per_month" => hours_per_month,
+      "hours_per_year" => hours_per_year
+    }
   end
 end
---
+++ lib/parser.ex
@@ -0,0 +1,33 @@
+defmodule GenReport.Parser do
+  @months [
+    "janeiro",
+    "fevereiro",
+    "março",
+    "abril",
+    "maio",
+    "junho",
+    "julho",
+    "agosto",
+    "setembro",
+    "outubro",
+    "novembro",
+    "dezembro"
+  ]
+
+  def parse_file(filename) do
+    "reports/#{filename}"
+    |> File.stream!()
+    |> Stream.map(&parse_line/1)
+  end
+
+  defp parse_line(line) do
+    line
+    |> String.trim()
+    |> String.split(",")
+    |> List.update_at(0, &String.downcase/1)
+    |> List.update_at(1, &String.to_integer/1)
+    |> List.update_at(2, &String.to_integer/1)
+    |> List.update_at(3, fn month -> Enum.at(@months, String.to_integer(month) - 1) end)
+    |> List.update_at(4, &String.to_integer/1)
+  end
+end
